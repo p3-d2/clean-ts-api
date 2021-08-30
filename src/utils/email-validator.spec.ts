f4cc295b066146087817f095cb3d133a4fa7374a
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-30 15:13:46 -0300

    refactor: change file name

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,6 +1,6 @@
 import validator from 'validator'
 
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 
 jest.mock('validator', () => ({
   isEmail (): boolean {
