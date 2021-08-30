2736330bcdad2a06b6f1ebcfaf65c5e681a59a7c
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-08-30 16:23:49 -0300

    test: remove index.ts and protocols from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
