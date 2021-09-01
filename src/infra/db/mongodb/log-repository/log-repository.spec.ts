98038815b08253537dfb875c7b264d0f266a9e53
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-01 18:22:53 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log-repository.spec.ts
@@ -0,0 +1,29 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+const makeSut = (): LogMongoRepository => new LogMongoRepository()
+
+describe('Account Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    errorCollection = MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
