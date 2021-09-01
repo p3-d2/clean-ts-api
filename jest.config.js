6c0f97aca8cfe6d25b1827cdd0e8a5e8bd376364
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-01 14:21:27 -0300

    chore: remove main folder from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/*-protocols.ts',
     '!<rootDir>/src/**/index.ts'
   ],
