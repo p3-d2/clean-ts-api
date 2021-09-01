733e609c336faa0678d1085965c2be5279a5784b
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-01 15:19:47 -0300

    refactor: make server error return the error stack

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack: string | null = null) {
     super('Internal server error')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
