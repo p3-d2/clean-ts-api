a82bb09ef6552446aa8217fd25dddc12f7e43a2c
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-11 12:44:24 -0300

    refactor: move password comparation to the ValidationComposite

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -30,10 +30,7 @@ export class SignUpController implements Controller {
       if (error) {
         return badRequest(error)
       }
-      const { name, password, passwordConfirmation, email } = httpRequest.body
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
-      }
+      const { name, email, password } = httpRequest.body
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
