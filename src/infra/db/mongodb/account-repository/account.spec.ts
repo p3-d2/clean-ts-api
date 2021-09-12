36bccd5decbf15005e017a9cc0142c8b32fef79a
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-11 23:48:00 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -54,4 +54,19 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const { insertedId } = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    let account = await accountCollection.findOne({ _id: insertedId })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBeFalsy()
+    await sut.updateAccessToken(account._id, 'any_token')
+    account = await accountCollection.findOne({ _id: insertedId })
+    expect(account.accessToken).toBe('any_token')
+  })
 })
