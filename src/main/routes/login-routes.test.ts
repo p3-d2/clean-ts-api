0da4c2959e9bdd3bcc4bba65c38a921aaed32916
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-14 00:05:14 -0300

    refactor: remove duplicated code

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -5,9 +5,18 @@ import { hash } from 'bcrypt'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
-describe('Login Routes', () => {
-  let accountCollection: Collection
+let accountCollection: Collection
 
+const insertFakeUser = async (): Promise<void> => {
+  const password = await hash('any_password', 12)
+  await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password
+  })
+}
+
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -35,12 +44,7 @@ describe('Login Routes', () => {
     })
 
     test('Should return 403 if email is already in use', async () => {
-      const password = await hash('any_password', 12)
-      await accountCollection.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password
-      })
+      await insertFakeUser()
       await request(app)
         .post('/api/signup')
         .send({
@@ -55,12 +59,7 @@ describe('Login Routes', () => {
 
   describe('POST /login', () => {
     test('Should return 200 on login', async () => {
-      const password = await hash('any_password', 12)
-      await accountCollection.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password
-      })
+      await insertFakeUser()
       await request(app)
         .post('/api/login')
         .send({
