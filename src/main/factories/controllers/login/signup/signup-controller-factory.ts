999256e76d80e04f8285ab479f2c7b0e81b6c462
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-16 12:22:27 -0300

    fix: fix usecases factories imports

--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from '../../../../../presentation/controllers/login/signup/signup-controller'
 import { Controller } from '../../../../../presentation/protocols'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
-import { makeDbAddAccount } from '../../../usecases/add-account/db-add-account-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
+import { makeDbAddAccount } from '../../../usecases/account/add-account/db-add-account-factory'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
 
 export const makeSignUpController = (): Controller => {
