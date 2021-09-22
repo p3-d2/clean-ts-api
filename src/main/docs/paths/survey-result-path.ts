9ea1e0148b4116d4c808cea9ddcecf67abca3cb4
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-09-22 17:41:57 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -46,5 +46,43 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Survey'],
+    summary: 'API to query the results of a survey',
+    description: 'This route can be run by **authenticated users**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID of the survey to be answered',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
