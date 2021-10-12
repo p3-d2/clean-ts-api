01fb48dfa606c9ddafc8d3ff2f141ac906461580
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-10-12 11:24:50 -0300

    feat: add signUp mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -2,7 +2,11 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   extend type Query {
-    login(email: String!, password: String!): Account!
+    login (email: String!, password: String!): Account!
+  }
+
+  extend type Mutation {
+    signUp (name: String!, email: String!, password: String!, passwordConfirmation: String): Account!
   }
   
   type Account {
