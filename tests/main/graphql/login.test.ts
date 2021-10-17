5a8b72565815f87fbd57d4ddc65973bf26346f1d
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-10-17 14:42:35 -0300

    test: ensure Login Query returns an account on valid credentials

---
+++ tests/main/graphql/login.test.ts
@@ -0,0 +1,51 @@
+import { MongoHelper } from '@/infra/db'
+import { setupApp } from '@/main/config/app'
+
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+import { Express } from 'express'
+import request from 'supertest'
+
+let accountCollection: Collection
+let app: Express
+
+describe('Login GraphQL', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Login Query', () => {
+    const query = `query {
+      login (email: "pedro.contato.email@gmail.com", password: "123") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid credentials', async () => {
+      const name = 'Pedro'
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name,
+        email: 'pedro.contato.email@gmail.com',
+        password
+      })
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.login.accessToken).toBeTruthy()
+      expect(res.body.data.login.name).toBe(name)
+    })
+  })
+})
