375d2e90d29a82a33c6f1903b91be7b6b0580c3c
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-10-18 12:46:11 -0300

    test: ensure Login Query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -47,5 +47,14 @@ describe('Login GraphQL', () => {
       expect(res.body.data.login.accessToken).toBeTruthy()
       expect(res.body.data.login.name).toBe(name)
     })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(401)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Unauthorized')
+    })
   })
 })
