522bf143f8d206249fdd04c4649f7ed960a7688d
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-10-18 12:55:59 -0300

    test: ensure SignUp Mutation returns an account on valid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -57,4 +57,22 @@ describe('Login GraphQL', () => {
       expect(res.body.errors[0].message).toBe('Unauthorized')
     })
   })
+
+  describe('SignUp Mutation', () => {
+    const query = `mutation {
+      signUp (name: "Pedro", email: "pedro.contato.email@gmail.com", password: "123", passwordConfirmation: "123") {
+        accessToken
+        name
+      }
+    }`
+
+    test('Should return an Account on valid data', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.signUp.accessToken).toBeTruthy()
+      expect(res.body.data.signUp.name).toBe('Pedro')
+    })
+  })
 })
