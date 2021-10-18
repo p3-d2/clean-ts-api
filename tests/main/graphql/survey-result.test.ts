ed08e3fe71690a11c49ff70dc7f4365bee28e409
Pedro-frontEnd <pedro.contato.email@gmail.com>
2021-10-18 13:48:48 -0300

    test: ensure SurveyResult Query returns SurveyResult on success

---
+++ tests/main/graphql/survey-result.test.ts
@@ -0,0 +1,95 @@
+import request from 'supertest'
+import { Express } from 'express'
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
+
+import { MongoHelper } from '@/infra/db'
+import env from '@/main/config/env'
+import { setupApp } from '@/main/config/app'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const mockAccessToken = async (): Promise<string> => {
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'Pedro',
+    email: 'pedro.contato.email@mail.com',
+    password: '123'
+  })
+  const accessToken = sign({ id: insertedId.toString() }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: insertedId
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
+
+describe('SurveyResult GraphQL', () => {
+  let app: Express
+
+  beforeAll(async () => {
+    app = await setupApp()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('SurveyResult Query', () => {
+    test('Should return SurveyResult', async () => {
+      const accessToken = await mockAccessToken()
+      const now = new Date()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+      const query = `query {
+        surveyResult (surveyId: "${surveyRes.insertedId.toHexString()}") {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }`
+      const res = await request(app)
+        .post('/graphql')
+        .set('x-access-token', accessToken)
+        .send({ query })
+      expect(res.status).toBe(200)
+      expect(res.body.data.surveyResult.question).toBe('Question')
+      expect(res.body.data.surveyResult.date).toBe(now.toISOString())
+      expect(res.body.data.surveyResult.answers).toEqual([{
+        answer: 'Answer 1',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }, {
+        answer: 'Answer 2',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }])
+    })
+  })
+})
